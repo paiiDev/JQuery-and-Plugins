ea8e50d5b643bac5af30aba208b1e9792317f68c
Paii <pyaepaikyaw@gmail.com>
2026-04-27 22:50:19 +0630

    style: formatting UI

--- js/ui.js
+++ js/ui.js
@@ -1,11 +1,10 @@
 function renderTasks(tasks) {
-    $("#tasklist").empty();
-
-    tasks.forEach(task => {
-        $("#taskList").append(`<li>${task.todo}</li>`)
-    });
+  $("#tasklist").empty();
+  tasks.forEach((task) => {
+    $("#taskList").append(`<li>${task.todo}</li>`);
+  });
 }
 
 function addTaskToUI(task) {
-    $("#taskList").append(`<li>${task.todo}</li>`)
+  $("#taskList").append(`<li>${task.todo}</li>`);
 }
